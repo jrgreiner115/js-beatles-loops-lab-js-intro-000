76ea2d4fb59a9f50c7f660aaf2dd01df6fb404e3
Joe Greiner <jrgreiner115@gmail.com>
2018-02-13 18:24:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
-  for (var i = 0; i<musicians.foo; i++) {array.push(musicians[i] + 'plays' + instruments)
+  for (var i = 0; i<musicians.foo; i++) {array.push(musicians[i] + 'plays' + instruments[i])
+}
 }
