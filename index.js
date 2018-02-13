1d1cb6355a8a2b2cc57b00e16a47ba4087aad25e
Joe Greiner <jrgreiner115@gmail.com>
2018-02-13 18:33:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ return array;
 function johnLennonFacts(array) {
   var final = [];
   var n = 0;
-  while (n < array.length;) {final.push(array.)
+  while (n < array.length;) {final.push(array[n] )
 }
